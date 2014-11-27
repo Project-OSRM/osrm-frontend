27f2e8cc3b25ea11483d481015332ac04b321954
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2014-11-27 23:13:13 +0100

    Make print map static

--- src/printing.js
+++ src/printing.js
@@ -8,7 +8,14 @@ var options = require('./options.js');
 var parsedOptions = links.parse(window.location.search);
 var viewOptions = L.extend(options.viewDefaults, parsedOptions);
 
-var map = L.map('map', {zoomControl: false}).setView(viewOptions.center, viewOptions.zoom);
+var map = L.map('map', {
+    zoomControl: false,
+    dragging: false,
+    scrollWheelZoom: false,
+    touchZoom: false,
+    doubleClickZoom: false
+  }
+  ).setView(viewOptions.center, viewOptions.zoom);
 
 var mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v3/dennisl.4e2aab76/{z}/{x}/{y}.png',
     {attribution: '&copy; <a href="http://mapbox.com/">MapBox</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
@@ -21,21 +28,14 @@ document.getElementById("instructions").appendChild(itineraryContainer);
 
 osrm.route(viewOptions.waypoints, function(error, alts) {
   var line = L.Routing.line(alts[0], {}),
+      bounds,
       i;
   line.addTo(map);
   map.fitBounds(line.getBounds());
+
   viewOptions.waypoints.map(function (wp) {
     L.marker(wp.latLng).addTo(map);
   });
   itinerary.setAlternatives(alts);
 }, null, {});
 
-/*
-var lrm = L.Routing.control({language: viewOptions.language,
-});
-lrm.addTo(map);
-
-
-lrm.setWaypoints();
-
-*/
