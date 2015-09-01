43bf9ba26724c44f79aa3b6ff19b98b1f5cc4677
amyleewalton <amylee@mapbox.com>
2015-09-01 16:53:59 -0400

    print working

--- printing.js
+++ printing.js
@@ -28,20 +28,23 @@ function makeIcon(i, n) {
   if (i === 0) {
     return L.icon({
       iconUrl: markerList[0],
-      iconSize: [25, 41]
+      iconSize: [20, 56],
+      iconAnchor: [10, 28]
     });
   } if (i === n - 1) {
     return L.icon({
       iconUrl: markerList[1],
-      iconSize: [25, 41]
+      iconSize: [20, 56],
+      iconAnchor: [10, 28]
     });
   } else {
     return L.icon({
       iconUrl: url,
-      iconSize: [25, 41]
+      iconSize: [20, 56],
+      iconAnchor: [10, 28]
     });
   }
-};
+}
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg', {
   attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">Mapbox</a>. ' +
@@ -52,22 +55,24 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@2x.png?a
 var osrm = L.Routing.osrm();
 var itinerary = L.Routing.itinerary({language: viewOptions.language});
 var itineraryContainer = itinerary.onAdd(map);
-
 document.getElementById("instructions").appendChild(itineraryContainer);
 
 osrm.route(viewOptions.waypoints, function(error, alts) {
+  var altIdx = viewOptions.alternative ? viewOptions.alternative : 0;
   var lineOptions = options.lrm.lineOptions;
   var line = L.Routing.line(alts[alts.length - 1], lineOptions);
   line.addTo(map);
   map.fitBounds(line.getBounds());
+
+  viewOptions.waypoints.map(function (currentVal, i, n) {
+    var options = { icon: makeIcon(currentVal, n.length) };
+    var colorMarkers = L.marker(currentVal.latLng, options);
+    colorMarkers.addTo(map);
+  });
+  itinerary.setAlternatives(alts);
+  itinerary.selectAlternative(altIdx);
 });
 
-viewOptions.waypoints.map(function (currentVal, i, n) {
-  var options = { icon: makeIcon(currentVal, n.length) };
-  var colorMarkers = L.marker(currentVal.latLng, options);
-  colorMarkers.addTo(map);
-  itinerary.setAlternatives(alts);
-});
 
 
 
