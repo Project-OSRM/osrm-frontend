bf5ec47b31a4add6932c79fd4e32f0140afc3cd2
Lauren Budorick <lauren@mapbox.com>
2015-09-10 16:03:01 -0700

    hints :)

--- src/index.js
+++ src/index.js
@@ -169,7 +169,11 @@ if (viewOptions.waypoints.length > 1) {
 }
 
 plan.on('waypointgeocoded', function(e) {
-  console.log(e);
+  if (plan._waypoints.filter(function(wp) { return !!wp.latLng; }).length < 2) {
+    console.log('only one!', e);
+  } else {
+    console.log('got both :sunglasses:', e)
+  }
 });
 
 // add onClick event
