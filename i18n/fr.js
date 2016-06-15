c0cdc99d648a77c23190619695901ec12b145cb5
Yves Moisan <ymoisan@users.noreply.github.com>
2016-06-15 10:00:21 -0400

    Added French translation

---
+++ i18n/fr.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'Français',
+  key: 'fr',
+  'Open in editor': 'Ouvrir dans l\'éditeur',
+  'Open in JOSM': 'Ouvrir dans JOSM',
+  'Select language': 'Choisir la langue',
+  'Start - press enter to drop marker': 'Début - appuyez sur la touche Retour pour ajouter un marqueur',
+  'End - press enter to drop marker': 'Fin - appuyez sur la touche Retour pour ajouter un marqueur',
+  'Via point - press enter to drop marker': 'Étape intermédiaire - appuyez sur la touche Retour pour ajouter un marqueur'
+};
