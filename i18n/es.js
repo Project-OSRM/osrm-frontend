2a770b0686f99ae57eda95145a942b65e84865db
Igor Támara <igor@tamarapatino.org>
2016-11-09 20:27:31 -0500

    Added es language

---
+++ i18n/es.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Español',
+  key: 'es',
+  'Open in Debug Map': 'Abrir en mapa depuración',
+  'Open in Mapillary': 'Abrir en Mapillary',
+  'Open in editor': 'Abrir en editor',
+  'Open in JOSM': 'Abrir en JOSM',
+  'Select language': 'Seleccionar idioma',
+  'Start - press enter to drop marker': 'Incio - presione enter para colocar el marcador',
+  'End - press enter to drop marker': 'Fin - presione enter para colocar el marcador',
+  'Via point - press enter to drop marker': 'Punto en la vía - presione enter para colocar un marcador'
+};
