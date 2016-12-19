9d46b28e2a394f187dd6adb1f46d196888679ebe
riiga <riiga_92@hotmail.com>
2016-12-19 20:37:51 +0100

    Create sv.js

---
+++ i18n/sv.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Svenska',
+  key: 'sv',
+  'Open in Debug Map': 'Öppna i Debug Map',
+  'Open in Mapillary': 'Öppna i Mapillary',
+  'Open in editor': 'Öppna i redigerare',
+  'Open in JOSM': 'Öppna i JOSM',
+  'Select language': 'Välj språk',
+  'Start - press enter to drop marker': 'Start - tryck för att placera startpunkt',
+  'End - press enter to drop marker': 'Mål - tryck för att placera slutpunkt',
+  'Via point - press enter to drop marker': 'Via - tryck för att placera delmålspunkt'
+};
