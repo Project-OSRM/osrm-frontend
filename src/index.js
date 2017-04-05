cd5474fff7bfdc7e148ad994fed4d23873433acc
AnixPasBesoin <anispasbesoin@hotmail.com>
2017-04-05 10:59:08 +0200

    Yet another modification to the addWaypoint function
    Since the addWaypoint function deals with waypoints, it makes more sens to pass it a waypoint as an argument, not the click event.
    IMO, this makes the code more cleaner.

--- src/index.js
+++ src/index.js
@@ -182,13 +182,14 @@ plan.on('waypointgeocoded', function(e) {
 });
 
 // add onClick event
-map.on('click', addWaypoint);
-function addWaypoint(e) {
+map.on('click', function (e){
+  addWaypoint(e.latlng);
+});
+function addWaypoint(waypoint) {
   var length = lrmControl.getWaypoints().filter(function(pnt) {
     return pnt.latLng;
   });
   length = length.length;
-  var waypoint = e.latlng;
   if (!length) {
     lrmControl.spliceWaypoints(0, 1, waypoint);
   } else {
