f14cfc6a4f0065beaac9cccf6e05543f1ff06a29
Minh Nguyễn <mxn@1ec5.org>
2017-05-04 08:53:45 -0700

    Added Vietnamese localization (#240)

---
+++ i18n/vi.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Tiếng Việt',
+  key: 'vi',
+  'Open in Debug Map': 'Mở trong Bản đồ Gỡ lỗi',
+  'Open in Mapillary': 'Mở trong Mapillary',
+  'Open in editor': 'Mở trong chương trình vẽ',
+  'Open in JOSM': 'Mở trong JOSM',
+  'Select language': 'Chọn ngôn ngữ',
+  'Start - press enter to drop marker': 'Khởi hành – bấm Enter để thả ghim',
+  'End - press enter to drop marker': 'Đến – bấm Enter để thả ghim',
+  'Via point - press enter to drop marker': 'Qua – bấm Enter để thả ghim'
+};
