10ed8f362e21680c1262c97f90f5b883d5f96d0a
Huyen Chau Nguyen <hello@chau-nguyen.de>
2018-04-09 12:45:00 +0200

    fix debug link

--- src/tools.js
+++ src/tools.js
@@ -77,7 +77,7 @@ var Control = L.Control.extend({
     var position = this._map.getCenter(),
       zoom = this._map.getZoom(),
       prec = 6;
-    window.open("https://map.project-osrm.org/debug/#" + zoom + "/" + position.lat.toFixed(prec) + "/" + position.lng.toFixed(prec));
+    window.open("debug/#" + zoom + "/" + position.lat.toFixed(prec) + "/" + position.lng.toFixed(prec));
   },
 
   _openMapillary: function() {
