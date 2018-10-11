75252d449cb708d4fc5e78b4c28823d8dea84694
Marco Ciampa <ciampix@posteo.net>
2018-10-11 12:58:10 +0200

    Updated Italian translation

---
+++ i18n/it.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'Italiano',
+  key: 'it',
+  'Open in Debug Map': 'Apri nella mappa di Debug',
+  'Open in Mapillary': 'Apri in Mapillary',
+  'GPX': 'Esporta il file GPX',
+  'Open in editor': 'Apri nell’editor',
+  'Open in JOSM': 'Apri in JOSM',
+  'Select language': 'Seleziona la lingua',
+  'Start - press enter to drop marker': 'Inizio - premere invio per rilasciare un marcatore',
+  'End - press enter to drop marker': 'Fine - premere invio per rilasciare un marcatore',
+  'Via point - press enter to drop marker': 'Punto di passaggio - premere invio per rilasciare un marcatore'
+};
