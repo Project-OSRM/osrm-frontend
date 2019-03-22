131822276a5f6c732cc9746a650795c9b77bc890
Josh Andrews <joshphilipandrews@gmail.com>
2019-03-22 11:09:05 +0900

    Add Japanese Localization

---
+++ i18n/ja.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: '英語',
+  key: 'ja',
+  'Open in Debug Map': 'デバッグされた地図で開く',
+  'Open in Mapillary': 'Mapilaryで開く',
+  'GPX': 'ファイルを出力する',
+  'Open in editor': '編集モードで開く',
+  'Open in JOSM': 'JOSMで開く',
+  'Select language': '言語を選択',
+  'Start - press enter to drop marker': '出発地 – ピンを置くにはEnterを押して下さい',
+  'End - press enter to drop marker': '目的地-ピンを置くにはEnterを押して下さい ',
+  'Via point - press enter to drop marker': '経由地 -ピンを置くにはEnterを押して下さい'
+};
