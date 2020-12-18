f575d29655c294e36e06bb5e7415abc6146a2352
David Hummel <6109326+hummeltech@users.noreply.github.com>
2020-12-18 11:01:39 -0700

    Added ability to set Default Layer to other than `Mapbox Streets`
    * By defining an environment variable
      * `OSRM_DEFAULT_LAYER`

--- scripts/replace.js
+++ scripts/replace.js
@@ -19,12 +19,14 @@ for (const filepath of [leafletOptions, debug]) {
   const CENTER = process.env.OSRM_CENTER || '38.8995, -77.0269'
   const BACKEND = process.env.OSRM_BACKEND || 'https://router.project-osrm.org'
   const LANGUAGE = process.env.OSRM_LANGUAGE || 'en'
+  const DEFAULT_LAYER = process.env.OSRM_DEFAULT_LAYER || 'streets'
 
   // Edit Leaflet Options
   if (BACKEND) options = options.replace(/http[s]?:\/\/router\.project-osrm\.org/, BACKEND)
   if (LABEL) options = options.replace('Car (fastest)', LABEL)
   if (ZOOM) options = options.replace('zoom: 13', `zoom: ${ZOOM}`)
   if (LANGUAGE) options = options.replace(`language: 'en'`, `language: '${LANGUAGE}'`)
+  if (DEFAULT_LAYER) options = options.replace('layer: streets', `layer: ${DEFAULT_LAYER}`)
   if (CENTER) {
     const latLng = CENTER.split(/[, ]+/)
     const lat = latLng[0];
--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ var itineraryBuilder = require('./itinerary_builder')(mergedOptions.language);
 
 var mapLayer = leafletOptions.layer;
 var overlay = leafletOptions.overlay;
-var baselayer = ls.get('layer') ? mapLayer[0][ls.get('layer')] : mapLayer[0]['Mapbox Streets'];
+var baselayer = ls.get('layer') ? mapLayer[0][ls.get('layer')] : leafletOptions.defaultState.layer;
 var layers = ls.get('getOverlay') && [baselayer, overlay['Small Components']] || baselayer;
 var map = L.map('map', {
   zoomControl: true,
