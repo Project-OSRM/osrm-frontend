814ceeb2e9c46e08a62f13ce9c988843415744b3
Mathieu Brunot <mb.mathieu.brunot@gmail.com>
2021-04-06 21:25:42 +0200

    🐛 Update leaflet options (#330)
    * :bug: Update options to fix #329
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Fix default layer
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :sparkles: Replace MapBox token
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * ✏️ Fix typo in scripts/replace.js
    
    Co-authored-by: Matt Harding <matt@zippitycars.com>
    
    * :wrench: Use Mapbox streets as default
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * 💬 Add waymarkedtrails attribution
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * 🐛 Fix mapbox token replace
    
    Co-authored-by: Michael Bell <moke.byll@gmail.com>
    
    Co-authored-by: Matt Harding <matt@zippitycars.com>
    Co-authored-by: Michael Bell <moke.byll@gmail.com>

--- scripts/replace.js
+++ scripts/replace.js
@@ -20,6 +20,7 @@ for (const filepath of [leafletOptions, debug]) {
   const BACKEND = process.env.OSRM_BACKEND || 'https://router.project-osrm.org'
   const LANGUAGE = process.env.OSRM_LANGUAGE || 'en'
   const DEFAULT_LAYER = process.env.OSRM_DEFAULT_LAYER || 'streets'
+  const MAPBOX_TOKEN = process.env.OSRM_MAPBOX_TOKEN || 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg'
 
   // Edit Leaflet Options
   if (BACKEND) options = options.replace(/http[s]?:\/\/router\.project-osrm\.org/, BACKEND)
@@ -27,6 +28,7 @@ for (const filepath of [leafletOptions, debug]) {
   if (ZOOM) options = options.replace('zoom: 13', `zoom: ${ZOOM}`)
   if (LANGUAGE) options = options.replace(`language: 'en'`, `language: '${LANGUAGE}'`)
   if (DEFAULT_LAYER) options = options.replace('layer: streets', `layer: ${DEFAULT_LAYER}`)
+  if (MAPBOX_TOKEN) options = options.replace(/mapboxToken = '.*'/, `mapboxToken = '${MAPBOX_TOKEN}'`)
   if (CENTER) {
     const latLng = CENTER.split(/[, ]+/)
     const lat = latLng[0];
