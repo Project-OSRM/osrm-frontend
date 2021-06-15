0584c396d2a3f33420cfb5f137f5d8839774a3a6
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-06-15 12:48:05 +0900

    Fix the Japanese localization

--- i18n/ja.js
+++ i18n/ja.js
@@ -1,13 +1,13 @@
 module.exports = {
-  name: '英語',
+  name: '日本語',
   key: 'ja',
-  'Open in Debug Map': 'デバッグされた地図で開く',
-  'Open in Mapillary': 'Mapilaryで開く',
+  'Open in Debug Map': 'デバッグ地図で開く',
+  'Open in Mapillary': 'Mapillaryで開く',
   'GPX': 'ファイルを出力する',
   'Open in editor': '編集モードで開く',
   'Open in JOSM': 'JOSMで開く',
   'Select language': '言語を選択',
   'Start - press enter to drop marker': '出発地 – ピンを置くにはEnterを押して下さい',
-  'End - press enter to drop marker': '目的地-ピンを置くにはEnterを押して下さい ',
-  'Via point - press enter to drop marker': '経由地 -ピンを置くにはEnterを押して下さい'
+  'End - press enter to drop marker': '目的地 - ピンを置くにはEnterを押して下さい',
+  'Via point - press enter to drop marker': '経由地 - ピンを置くにはEnterを押してください'
 };
